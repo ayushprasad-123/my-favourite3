1d741eabb4add90a4b95b1ab093fa147dac3dc87
ayushprasad-123 <75791807+ayushprasad-123@users.noreply.github.com>
2021-01-16 11:49:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function ex(){
+  document.getElementById("bio").innerHTML="I like science because it helps us understand the world around us. Without science, we woudn't know why bees like flowers, or why we shiver, or really anything for that matter."
+}
